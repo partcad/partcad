f23bb05a83bcca6a83bbfc9dba3fe6e6ebebc566
Roman Kuzmenko <openvmp@proton.me>
2024-10-30 17:31:44 -0700

    Troubleshooting

--- partcad/src/partcad/sketch_factory_build123d.py
+++ partcad/src/partcad/sketch_factory_build123d.py
@@ -100,10 +100,10 @@ class SketchFactoryBuild123d(SketchFactoryPython):
 
             await self.runtime.ensure("ocp-tessellate")
             await self.runtime.ensure("cadquery")
-            # await self.runtime.ensure("numpy==1.24.1")
-            # await self.runtime.ensure("numpy-quaternion==2023.0.4")
-            # await self.runtime.ensure("nptyping==1.24.1")
-            # await self.runtime.ensure("typing_extensions>=4.6.0,<5")
+            await self.runtime.ensure("numpy==1.24.1")
+            await self.runtime.ensure("numpy-quaternion==2023.0.4")
+            await self.runtime.ensure("nptyping==1.24.1")
+            await self.runtime.ensure("typing_extensions>=4.6.0,<5")
             await self.runtime.ensure("build123d>=0.7.0")
             cwd = self.project.config_dir
             if self.cwd is not None:
