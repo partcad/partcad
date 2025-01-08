22375623c57e6a9281f9edf714d6b55211b699fc
Roman Kuzmenko <rkuz@partcad.org>
2025-01-08 00:14:32 +0000

    feat(partcad): Refactor mesh generation and tests
    - Refactored mesh generation in shape.py to use `theLinDeflection` and `theAngDeflection` for better control.
    - Removed unused `Path` import in shape_ai.py.
    - Updated `utils_ocp.py` to use the new mesh generation parameters.
    - Removed redundant `get_cadquery` calls from assembly tests.
    - Modified context tests to use `get_wrapped` instead of `get_cadquery`.

--- partcad/tests/unit/test_assembly.py
+++ partcad/tests/unit/test_assembly.py
@@ -26,7 +26,6 @@ def test_assembly_primitive():
     model.add(part1, loc=pc.Location((0, 0, 0), (0, 0, 1), 0))
     model.add(part2, loc=pc.Location((0, 0, 1), (0, 0, 1), 0))
     assert asyncio.run(model.get_shape()) is not None
-    assert asyncio.run(model.get_cadquery()) is not None
     assert asyncio.run(model.get_build123d()) is not None
 
 
@@ -35,7 +34,6 @@ def test_assembly_example_assy_primitive():
     primitive = ctx._get_assembly("/produce_assembly_assy:primitive")
     assert primitive is not None
     assert asyncio.run(primitive.get_shape()) is not None
-    assert asyncio.run(primitive.get_cadquery()) is not None
     assert asyncio.run(primitive.get_build123d()) is not None
 
 
@@ -44,7 +42,6 @@ def test_assembly_example_assy_logo():
     logo = ctx._get_assembly("/produce_assembly_assy:logo")
     assert logo is not None
     assert asyncio.run(logo.get_shape()) is not None
-    assert asyncio.run(logo.get_cadquery()) is not None
     assert asyncio.run(logo.get_build123d()) is not None
 
 
@@ -53,7 +50,6 @@ def test_assembly_example_assy_logo_embedded():
     logo = ctx._get_assembly("/produce_assembly_assy:logo_embedded")
     assert logo is not None
     assert asyncio.run(logo.get_shape()) is not None
-    assert asyncio.run(logo.get_cadquery()) is not None
     assert asyncio.run(logo.get_build123d()) is not None
 
 
