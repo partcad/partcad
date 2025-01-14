6a908266fff415cc3570a3cbfd855ba860b8b895
Roman Kuzmenko <rkuz@partcad.org>
2025-01-14 02:32:52 +0000

    feat(devcontainer): Update Python version to 3.11 for compatibility
    Update .devcontainer/requirements.txt to specify Python 3.10-3.13
    
    Update partcad/requirements.txt to include cadquery-ocp==7.7.2 and ocpsvg==0.3.4
    
    Update part_factory_build123d.py to support Python 3.11
    
    Update part_factory_cadquery.py to support Python 3.11
    
    Update part_factory_step.py to support Python 3.11
    
    Update provider_factory_python.py to support Python 3.11
    
    Update runtime_python.py to support Python 3.11,  adjusting packages for improved compatibility
    
    Update shape.py to support Python 3.11 and adjust packages for improved compatibility
    
    Update sketch_factory_build123d.py to support Python 3.11, adjusting packages
    
    Update sketch_factory_cadquery.py to support Python 3.11, adjusting packages
    
    Update sketch_factory_dxf.py to support Python 3.11
    
    Update poetry.lock for updated package versions
    
    Update pyproject.toml with updated package versions and specifications

--- partcad/src/partcad/provider_factory_python.py
+++ partcad/src/partcad/provider_factory_python.py
@@ -51,10 +51,10 @@ class ProviderFactoryPython(ProviderFactoryFile):
             python_version = self.project.python_version
         if python_version is None:
             # Stay one step ahead of the minimum required Python version
-            python_version = "3.10"
-        if python_version == "3.12" or python_version == "3.11":
-            pc_logging.debug("Downgrading Python version to 3.10 to minimize compatibility issues")
-            python_version = "3.10"
+            python_version = "3.11"
+        if python_version == "3.12" or python_version == "3.10":
+            pc_logging.debug("Downgrading Python version to 3.11 to minimize compatibility issues")
+            python_version = "3.11"
 
         self.runtime = self.ctx.get_python_runtime(python_version)
         self.session = self.runtime.get_session(source_project.name)
@@ -135,36 +135,27 @@ class ProviderFactoryPython(ProviderFactoryFile):
             # TODO-200: Create a version resolution mechanism that can handle dependency conflicts
             # TODO-201: Implement a version update strategy for security patches
             await self.runtime.ensure_async(
-                "ocp-tessellate==3.0.8",
+                "ocp-tessellate==3.0.9",
                 session=self.session,
             )
             await self.runtime.ensure_async(
-                "nlopt==2.7.1",
+                "nlopt==2.9.0",
+                session=self.session,
+            )
+            await self.runtime.ensure_async(
+                "cadquery==2.5.2",
+                session=self.session,
+            )
+            await self.runtime.ensure_async(
+                "numpy==2.2.1",
+                session=self.session,
+            )
+            await self.runtime.ensure_async(
+                "typing_extensions==4.12.2",
                 session=self.session,
             )
             await self.runtime.ensure_async(
                 "cadquery-ocp==7.7.2",
-                session=self.session,
-            )
-            await self.runtime.ensure_async(
-                "cadquery==2.4.0",
-                session=self.session,
-            )
-            await self.runtime.ensure_async(
-                "numpy==1.26.4",
-                session=self.session,
-            )
-            await self.runtime.ensure_async(
-                "numpy-quaternion==2023.0.4",
-                session=self.session,
-            )
-            await self.runtime.ensure_async(
-                "nptyping==2.0.1",
-                session=self.session,
-            )
-            await self.runtime.ensure_async(
-                # "typing_extensions>=4.6.0,<5", # doesn't work on Windows
-                "typing_extensions==4.12.2",
                 session=self.session,
             )
             cwd = self.project.config_dir
