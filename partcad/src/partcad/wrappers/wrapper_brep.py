3fdf66534b7f27a5da39a53bff8b1efe3857f967
Bohdan Hnydyn <bgnydyn@gmail.com>
2025-01-14 20:25:11 +0000

    fix(wrapper_brep): Remove request parameter from process function

--- partcad/src/partcad/wrappers/wrapper_brep.py
+++ partcad/src/partcad/wrappers/wrapper_brep.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(__file__))
 import wrapper_common
 
 
-def process(path, request):
+def process(path):
     try:
         shape = TopoDS_Shape()
         builder = BRep_Builder()
