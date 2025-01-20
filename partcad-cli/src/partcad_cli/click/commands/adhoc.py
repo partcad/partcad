6feb9c4ba60c9d64e319d47649aea1a2599f2505
Bohdan Hnydyn <bgnydyn@gmail.com>
2025-01-20 15:12:29 +0000

    feat: add help message for 'pc addhoc'

--- partcad-cli/src/partcad_cli/click/commands/adhoc.py
+++ partcad-cli/src/partcad_cli/click/commands/adhoc.py
@@ -7,6 +7,6 @@ class AdhocCommands(Loader):
     COMMANDS_FOLDER = os.path.join(Loader.COMMANDS_FOLDER, "adhoc")
 
 
-@click.command(cls=AdhocCommands, help="???")
+@click.command(cls=AdhocCommands, help="Run ad-hoc commands for on-the-fly operations without requiring a full configuration or setup.")
 def cli() -> None:
     pass
