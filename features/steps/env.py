c6446a328ca178124fd097bccc733240b544bfac
Roman Kuzmenko <rkuz@partcad.org>
2025-02-06 20:00:39 +0000

    Fixing build

---
+++ features/steps/env.py
@@ -0,0 +1,14 @@
+from behave import when, given
+from behave.runner import Context
+import logging
+from features.utils import expandvars  # type: ignore # TODO: @alexanderilyin python.autoComplete.extraPaths
+
+
+@given('environment variable "{variable}" is set to "{value}"')
+def environment_variable_is_set_to(context: Context, variable: str, value: str):
+    value = expandvars(value, context)
+    logging.debug(f"Set environment variable {variable} to {value}")
+
+    if not hasattr(context, "env"):
+        context.env = {}
+    context.env[variable] = value
