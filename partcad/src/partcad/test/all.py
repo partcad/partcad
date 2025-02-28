53b13965c2962b8950fd595bb508753d85921798
Amha Kindu <amhaznif@gmail.com>
2025-02-28 22:15:06 +0000

    fix(CI): handle test lock up
    - Used semaphores on partcad tests to control the maximum concurrent tests
    - Skip testing & rendering kicad type objects
    - Introduced finalized flag to shape to avoid to skip operations non-finalized objects

--- partcad/src/partcad/test/all.py
+++ partcad/src/partcad/test/all.py
@@ -6,6 +6,7 @@
 #
 # Licensed under Apache License, Version 2.0.
 #
+import os
 
 from .test import Test
 from .cad import CadTest
@@ -17,7 +18,10 @@ from .cam_forming import CamFormingTest
 _global_tests: list[Test] = []
 
 
-def tests() -> list[Test]:
+def tests(concurrency_cap: int) -> list[Test]:
+    if concurrency_cap is None:
+        concurrency_cap = max(os.cpu_count(), 8)
+    Test.MAX_CONCURRENT_TESTS = concurrency_cap
     if len(_global_tests) == 0:
         _global_tests.extend(
             [
